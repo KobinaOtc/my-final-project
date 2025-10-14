47294dee42ccf254e56e92e75d6149f4b4a40b9b
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-10-14 23:18:03 +0000

    debugging

--- src/js/views.js
+++ src/js/views.js
@@ -1,17 +1,6 @@
-import { loadTemplate } from "./utils";
-
-export const renderHome = () => {
-    return loadTemplate('../views/home.html');
-}
-
-export const renderForms = () => {
-    return loadTemplate('../views/forms.html');
-}
-
-export const renderAbout = () => {
-    return loadTemplate('../views/about.html');
-}
-
-export const renderNews = () => {
-    return loadTemplate('../views/news.html')
-}
+export const views = {
+    home: '../views/home.html',
+    forms: '../views/forms.html',
+    about: '../views/about.html',
+    news: '../views/news.html'
+};
