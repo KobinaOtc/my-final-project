037a53ae80f8f2dde88203fc241f25f62f79a817
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-10-15 23:42:51 +0000

    deployment settings

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: '/my-final-project/',
     build: {
         rollupOptions:{
-            main: 'src/index.html'
+            main: '/index.html'
         }
     }
 })
