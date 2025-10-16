022b751f40e4e2f64c9ed5f848780d6d3b208239
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-10-16 02:36:39 +0000

    netlify set up

--- src/js/views.js
+++ src/js/views.js
@@ -1,6 +1,5 @@
 export const views = {
-    home: '/src/views/home.html',
-    forms: '/src/views/forms.html',
-    about: '/src/views/about.html',
-    news: '/src/views/news.html'
+    home: '/home.html',
+    about: '/about.html',
+    news: '/news.html'
 };
