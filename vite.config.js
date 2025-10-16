fe78dbd306d743f26ba4138a6f6c30cb20fb5c3c
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-10-16 02:04:38 +0000

    netlify set up

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/my-final-project/',
-    build: {
-        rollupOptions:{
-            main: '/index.html'
-        }
-    }
+    base: '/',
 })
